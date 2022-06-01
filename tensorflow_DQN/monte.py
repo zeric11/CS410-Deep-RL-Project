9f5de35a531bbe38de9fbd5f902f84d30b607068
chi-ha-ru <72625191+chi-ha-ru@users.noreply.github.com>
2022-05-31 20:35:37 -0700

    Update monte.py

--- tensorflow_DQN/monte.py
+++ tensorflow_DQN/monte.py
@@ -18,14 +18,8 @@ tf.disable_v2_behavior()
 import reinforcement_learning as rl
 env_name = 'ALE/MontezumaRevenge-v5'
 #env_name = 'ALE/MsPacman-v5'
-#env_name = 'ALE/SpaceInvaders-v5'
-#env_name = gym.make('Breakout-v0',frameskip=4,render_mode='human') #,obs_type='rgb',frameskip=5,mode=0,difficulty=0,repeat_action_probability=0.25,full_action_space=True,render_mode='human')#ALE/Breakout-v5')
-#env_name = 'MontezumaRevenge-v0'
-#env_name = 'SpaceInvaders-v0'
-#env_name = 'Breakout-v0'
 
 rl.checkpoint_base_dir = 'checkpoints_tutorial16/'
-
 rl.update_paths(env_name=env_name)
 
 agent = rl.Agent(env_name=env_name,
@@ -44,12 +38,6 @@ log_reward = rl.LogReward()
 #log_q_values.read()
 #print(log_reward)
 log_reward.read()
-
-#plt.plot(log_reward.count_states, log_reward.episode, label='Episode Reward')
-#plt.plot(log_reward.count_states, log_reward.mean, label='Mean of 100 episodes')
-#plt.xlabel('State-Count for Game Environment')
-#plt.legend()
-#plt.show()
 
 #Testing
 print('Testing')
